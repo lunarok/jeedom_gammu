4a27f67994eee7ce5b638052a647a7bc27ce98e7
Cédric Dupont <lunarok@gmail.com>
2016-05-03 12:15:29 +0200

    prise en compte apache pour le script SMS received

--- resources/install.sh
+++ resources/install.sh
@@ -91,4 +91,31 @@ sed -i -e 's/#USB#/'${escaped}'/g' /etc/gammu-smsdrc
 
 sed -i -e 's/#PIN#/'${escaped}'/g' /etc/gammu-smsdrc
 
+escaped="$1"
+
+    # escape all backslashes first
+    escaped="${escaped//\\/\\\\}"
+
+    # escape slashes
+    escaped="${escaped//\//\\/}"
+
+    # escape asterisks
+    escaped="${escaped//\*/\\*}"
+
+    # escape full stops
+    escaped="${escaped//./\\.}"
+
+    # escape [ and ]
+    escaped="${escaped//\[/\\[}"
+    escaped="${escaped//\[/\\]}"
+
+    # escape ^ and $
+    escaped="${escaped//^/\\^}"
+    escaped="${escaped//\$/\\\$}"
+
+    # remove newlines
+    escaped="${escaped//[$'\n']/}"
+
+sed -i -e 's/#DIR#/'${escaped}'/g' /var/spool/gammu/SMSDreceive.sh
+
 service gammu-smsd restart

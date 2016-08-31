67aea05d913d1b5c2be2cd6c09cdf2d9f49040ab
Cédric Dupont <lunarok@gmail.com>
2016-08-31 10:43:08 +0200

    page equipement correction onglet

--- desktop/php/gammu.php
+++ desktop/php/gammu.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!isConnect('admin')) {
-    throw new Exception('{{401 - Accès non autorisé}}');
+  throw new Exception('{{401 - Accès non autorisé}}');
 }
 sendVarToJS('eqType', 'gammu');
 $eqLogics = eqLogic::byType('gammu');
@@ -9,56 +9,56 @@ $eqLogics = eqLogic::byType('gammu');
 ?>
 
 <div class="row row-overflow">
-    <div class="col-lg-2 col-md-3 col-sm-4">
-        <div class="bs-sidebar">
-            <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
-                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un équipement}}</a>
-                <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-                <?php
-                foreach ($eqLogics as $eqLogic) {
-                    echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
-                }
-                ?>
-            </ul>
-        </div>
+  <div class="col-lg-2 col-md-3 col-sm-4">
+    <div class="bs-sidebar">
+      <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
+        <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un équipement}}</a>
+        <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+        <?php
+        foreach ($eqLogics as $eqLogic) {
+          echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
+        }
+        ?>
+      </ul>
+    </div>
+  </div>
+
+  <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+    <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
+    <div class="eqLogicThumbnailContainer">
+      <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
+        <center>
+          <i class="fa fa-wrench" style="font-size : 5em;color:#767676;"></i>
+        </center>
+        <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
+      </div>
     </div>
 
-    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-   <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
-   <div class="eqLogicThumbnailContainer">
-     <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
-     <center>
-       <i class="fa fa-wrench" style="font-size : 5em;color:#767676;"></i>
-     </center>
-     <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>{{Configuration}}</center></span>
-   </div>
- </div>
 
-
-        <legend><i class="fa fa-table"></i>  {{Mes Gammu}}
-        </legend>
-            <div class="eqLogicThumbnailContainer">
-                      <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-           <center>
-            <i class="fa fa-plus-circle" style="font-size : 7em;color:#00979c;"></i>
+    <legend><i class="fa fa-table"></i>  {{Mes Gammu}}
+    </legend>
+    <div class="eqLogicThumbnailContainer">
+      <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+        <center>
+          <i class="fa fa-plus-circle" style="font-size : 7em;color:#00979c;"></i>
         </center>
         <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>Ajouter</center></span>
+      </div>
+      <?php
+      foreach ($eqLogics as $eqLogic) {
+        $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
+        echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
+        echo "<center>";
+        echo '<img src="plugins/gammu/doc/images/gammu_icon.png" height="105" width="95" />';
+        echo "</center>";
+        echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+        echo '</div>';
+      }
+      ?>
     </div>
-                <?php
-                foreach ($eqLogics as $eqLogic) {
-                  $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
-                    echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
-                    echo "<center>";
-                    echo '<img src="plugins/gammu/doc/images/gammu_icon.png" height="105" width="95" />';
-                    echo "</center>";
-                    echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
-                    echo '</div>';
-                }
-                ?>
-            </div>
-    </div>
+  </div>
 
-<div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+  <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
 
     <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
     <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
@@ -69,31 +69,31 @@ $eqLogics = eqLogic::byType('gammu');
     </ul>
 
     <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
-<div role="tabpanel" class="tab-pane active" id="eqlogictab">
-    <form class="form-horizontal">
-      <fieldset>
-        <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-        <div class="form-group">
-          <label class="col-sm-3 control-label">{{Nom de l'équipement Gammu}}</label>
-          <div class="col-sm-3">
-            <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-            <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement gammu}}"/>
-          </div>
-        </div>
-        <div class="form-group">
-          <label class="col-sm-3 control-label" >Objet parent</label>
-          <div class="col-sm-3">
-              <select class="eqLogicAttr form-control" data-l1key="object_id">
-                   <option value="">Aucun</option>
-                   <?php
-  foreach (object::all() as $object) {
-  	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
-  }
-  ?>
-              </select>
-          </div>
-       </div>
-        <div class="form-group">
+      <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+        <form class="form-horizontal">
+          <fieldset>
+            <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+            <div class="form-group">
+              <label class="col-sm-3 control-label">{{Nom de l'équipement Gammu}}</label>
+              <div class="col-sm-3">
+                <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement gammu}}"/>
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-sm-3 control-label" >Objet parent</label>
+              <div class="col-sm-3">
+                <select class="eqLogicAttr form-control" data-l1key="object_id">
+                  <option value="">Aucun</option>
+                  <?php
+                  foreach (object::all() as $object) {
+                    echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+                  }
+                  ?>
+                </select>
+              </div>
+            </div>
+            <div class="form-group">
               <label class="col-sm-3 control-label">{{Catégorie}}</label>
               <div class="col-sm-8">
                 <?php
@@ -112,46 +112,51 @@ $eqLogics = eqLogic::byType('gammu');
                 <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
                 <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
               </div>
-</div>
+            </div>
 
-        <div class="form-group">
-        <label class="col-sm-3 control-label">{{Téléphone}} :</label>
-         <div class="col-sm-3">
-           <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="phone" placeholder="{{numéro}}"/>
-         </div>
-       </div>
+            <div class="form-group">
+              <label class="col-sm-3 control-label">{{Téléphone}} :</label>
+              <div class="col-sm-3">
+                <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="phone" placeholder="{{numéro}}"/>
+              </div>
+            </div>
 
-       <div class="form-group">
-       <label class="col-sm-3 control-label">{{Utilisateur}} :</label>
-        <div class="col-sm-3">
-          <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="user" placeholder="{{username}}"/>
-        </div>
+            <div class="form-group">
+              <label class="col-sm-3 control-label">{{Utilisateur}} :</label>
+              <div class="col-sm-3">
+                <input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="user" placeholder="{{username}}"/>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label class="col-sm-3 control-label">{{Interactions}} :</label>
+              <div class="col-sm-3">
+                <select class="form-control eqLogicAttr configuration" data-l1key="configuration" data-l2key="interact">
+                  <option value="0">{{Non}}</option>
+                  <option value="1">{{Oui}}</option>
+                </select>
+              </div>
+            </div>
+
+          </fieldset>
+        </form>
       </div>
 
-      <div class="form-group">
-      <label class="col-sm-3 control-label">{{Interactions}} :</label>
-      <div class="col-sm-3">
-        <select class="form-control eqLogicAttr configuration" data-l1key="configuration" data-l2key="interact">
-          <option value="0">{{Non}}</option>
-          <option value="1">{{Oui}}</option>
-        </select>
-        </div>
-       <div role="tabpanel" class="tab-pane" id="commandtab">
-  <form class="form-horizontal">
+      <div role="tabpanel" class="tab-pane" id="commandtab">
 
-  <table id="table_cmd" class="table table-bordered table-condensed">
-    <thead>
-      <tr>
-        <th>{{Nom}}</th><th>{{Valeur}}</th><th>{{Options}}</th>
-      </tr>
-    </thead>
-    <tbody>
-    </tbody>
-  </table>
+        <table id="table_cmd" class="table table-bordered table-condensed">
+          <thead>
+            <tr>
+              <th>{{Nom}}</th><th>{{Valeur}}</th><th>{{Options}}</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
 
-</div>
-</div>
-</div>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php include_file('desktop', 'gammu', 'js', 'gammu'); ?>
